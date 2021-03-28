85aebf0cc6628b95debccf0e76e2e30bc5a8ad86
kimkyury <mandarining0918@gmail.com>
2021-03-28 18:18:35 +0900

    DONE us[User model]

--- users/models.py
+++ users/models.py
@@ -1,18 +1,37 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-# Create your models here.
 
 class User(AbstractUser):
+    """ Custom User Model """   # docstring, 이 클래스가 쓰일때 마우스에 갖다대면 이 설명문이 표시됨
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_OTHER = "other"
+    GENDER_CHOICES = (
+        (GENDER_MALE, "Male"),
+        (GENDER_FEMALE, "Female"),
+        (GENDER_OTHER, "Other")
+        )
     
-    """ User Model """   # 이건 docstring임. 파이썬에쓰는 표준형식
-                         # 클래스생성시에 넣어서 뭔지 말해주는 거임.
-                         # 이걸 가지고 간 admin에서 이름에 마우스 갖다대면
-                         # 이 설명문이 뜬 단 말씀
+    LANGUAGE_ENGLISH = "english"
+    LANGUAGE_KOREAN = "korean"
+    LANGUAGE_CHOICES = (
+        (LANGUAGE_ENGLISH, "en"), (LANGUAGE_KOREAN, "ko")
+        )
 
+    CURRENCY_USD = "usd"
+    CURRENCY_KRW = "krw"
+    CURRENCY_CHOICES = (
+        (CURRENCY_USD, "USD"), (CURRENCY_KRW, "KRW"))
 
-
-    avatar = models.ImageField(null=True)
-    gender = models.CharField(max_length=10, null=True) # 최대 글자 수 지정
-    bio = models.TextField(default="")
-
-
+    avatar = models.ImageField(null=True, blank=True)
+    gender = models.CharField(
+        choices=GENDER_CHOICES, max_length=10, null=True, blank=True) # 최대 글자 수 지정
+    bio = models.TextField(default="", blank=True)
+    
+    birthdate = models.DateField(null=True)
+    langaue = models.CharField(
+        choices=LANGUAGE_CHOICES, max_length=2, null=True, blank=True)
+    currency = models.CharField(
+        choices=CURRENCY_CHOICES, max_length=3, null=True, blank=True)
+    
+    superhost = models.BooleanField(default=False)
