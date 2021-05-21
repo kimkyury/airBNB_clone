1848a253ed9f50caa2c45c2d43f10347cf148c1b
kimkyury <mandarining0918@gmail.com>
2021-05-21 22:14:03 +0900

    Complete [Model_Room]

--- rooms/admin.py
+++ rooms/admin.py
@@ -4,10 +4,19 @@ from . import models
 
 @admin.register(models.RoomType, models.Facility, models.Amenity, models.HouseRule)
 class ItemAdmin(admin.ModelAdmin):
+    """ Itrem Admin Definition """
     pass
 
 
 @admin.register(models.Room)
-class RommAdmin(admin.ModelAdmin):
+class RoomAdmin(admin.ModelAdmin):
+    """ RoomAdmin Admin Definition """
+
+    pass
+
+
+@admin.register(models.Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    """"""
 
     pass
