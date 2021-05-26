65b3e06925b0212800d2eac76c2dec4ec228d8ec
kimkyury <mandarining0918@gmail.com>
2021-05-26 21:05:48 +0900

    Add filter [room]

--- rooms/admin.py
+++ rooms/admin.py
@@ -12,7 +12,20 @@ class ItemAdmin(admin.ModelAdmin):
 class RoomAdmin(admin.ModelAdmin):
     """ RoomAdmin Admin Definition """
 
-    pass
+    list_display = ("name",
+        "country",
+        "city",
+        "price",
+        "guests",
+        "beds",
+        "bedrooms",
+        "baths",
+        "check_in",
+        "check_out",
+        "instant_book",
+    )
+    list_filter = ("instant_book", "city", "country" )
+    search_fields =("city", "^host__username")
 
 
 @admin.register(models.Photo)
