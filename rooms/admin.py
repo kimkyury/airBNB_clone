f38757d45dfd867df8409cbed9023839d874476a
kimkyury <mandarining0918@gmail.com>
2021-06-02 21:24:04 +0900

    add colume [room]

--- rooms/admin.py
+++ rooms/admin.py
@@ -42,7 +42,13 @@ class RoomAdmin(admin.ModelAdmin):
         "check_in",
         "check_out",
         "instant_book",
+        "count_amenities",
     )
+
+    ordering = ("name", "price", "bedrooms")
+
+
+
     list_filter = (
         "instant_book", 
         "host__superhost",
@@ -56,11 +62,13 @@ class RoomAdmin(admin.ModelAdmin):
     )
     search_fields =("city", "^host__username")
 
-    filter_horizontal =(
-        "amenities",
-        "facilities",
-        "house_rules",
-    )
+    filter_horizontal =("amenities", "facilities", "house_rules",)
+
+    def count_amenities(self, obj):
+        print(obj.amenities.all())
+        return "Potato"
+
+    count_amenities.short_description = "hello sexy!"
 
 @admin.register(models.Photo)
 class PhotoAdmin(admin.ModelAdmin):
