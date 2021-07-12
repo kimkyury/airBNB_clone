550607534776311ad260078d2a99fcb7dd01ec0a
kimkyury <mandarining0918@gmail.com>
2021-07-12 17:58:52 +0900

    Feat: setting URL

--- config/urls.py
+++ config/urls.py
@@ -4,7 +4,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 
-urlpatterns = [path("", include("core.urls")), path("admin/", admin.site.urls)]
+urlpatterns = [
+    path("", include("core.urls", namespace="core")), 
+    path("admin/", admin.site.urls)]
 
 
 if settings.DEBUG:
--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,6 @@
-from django.urls import path
+from django.urls import path,incdlue
 from rooms import views as room_views
+app_name = "core"
 
 urlpatterns = [
-    path("",room_views.all_rooms)
-    ]
+    path("",room_views.all_rooms, name ="home")]
