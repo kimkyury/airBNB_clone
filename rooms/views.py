e32468b0261bb2058141e2b3c100e79b7a772955
kimkyury <mandarining0918@gmail.com>
2021-07-12 20:48:45 +0900

    Fix: use paginator from django library

--- rooms/views.py
+++ rooms/views.py
@@ -1,21 +1,15 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from . import models
 from math import ceil
 # Create your views here.
 
 def all_rooms(request):
-    page = int(request.GET.get("page", 1))
-    page_size = 10
-    limit = page_size * page
-    offset = limit - page_size
-    all_rooms = models.Room.objects.all()[offset:limit]
-    page_count = ceil(models.Room.objects.count() / page_size)
-    return render(request, "rooms/home.html",  
-    {
-        "potato": all_rooms, 
-        "page": page, 
-        "page_count": page_count,
-        "page_range": range(1, page_count),
-    },
-)
+    page = request.GET.get("page",1)
+    room_list = models.Room.objects.all() #쿼리셋 생성
+    paginator = Paginator(room_list, 10, orphans=5)
+    rooms = paginator.page(int(page))
+
+    return render(request, "rooms/home.html",  {"rooms": rooms})
+    
 
