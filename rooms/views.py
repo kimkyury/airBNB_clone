09b3bc832f705532bdf39a9a6233f43d1da20685
kimkyury <mandarining0918@gmail.com>
2021-07-13 19:56:06 +0900

    Fix: 빈페이지 예외처리

--- rooms/views.py
+++ rooms/views.py
@@ -1,15 +1,14 @@
-from django.shortcuts import render
-from django.core.paginator import Paginator
+from django.shortcuts import render, redirect
+from django.core.paginator import Paginator, EmptyPage
 from . import models
-from math import ceil
-# Create your views here.
+
 
 def all_rooms(request):
-    page = request.GET.get("page",1)
-    room_list = models.Room.objects.all() #쿼리셋 생성
+    page = request.GET.get("page", 1)
+    room_list = models.Room.objects.all()
     paginator = Paginator(room_list, 10, orphans=5)
-    rooms = paginator.page(int(page))
-
-    return render(request, "rooms/home.html",  {"rooms": rooms})
-    
-
+    try:
+        rooms = paginator.page(int(page))
+        return render(request, "rooms/home.html", {"page": rooms})
+    except EmptyPage:
+        return redirect("/")
