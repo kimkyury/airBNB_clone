dc3905e96a8daf872cb438a95fed5cddca5af26a
kimkyury <mandarining0918@gmail.com>
2021-07-13 20:23:18 +0900

    Fix: 클래스 기반으로 변경

--- rooms/views.py
+++ rooms/views.py
@@ -1,14 +1,14 @@
-from django.shortcuts import render, redirect
-from django.core.paginator import Paginator, EmptyPage
+
+from django.views.generic import ListView
 from . import models
 
 
-def all_rooms(request):
-    page = request.GET.get("page", 1)
-    room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 10, orphans=5)
-    try:
-        rooms = paginator.page(int(page))
-        return render(request, "rooms/home.html", {"page": rooms})
-    except EmptyPage:
-        return redirect("/")
+class HomeView(ListView):  # Lendering 할 필요가 없어졌음
+
+
+    """ HomeView Definition """
+
+    model = models.Room
+    paginate_by = 10
+    paginate_orphans = 5 #이게 뭔지 정확히 모르겠음
+    ordering = "created"
