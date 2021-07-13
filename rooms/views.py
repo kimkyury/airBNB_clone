ac51caf1615d6c02d04a615fec42eca2bf297081
kimkyury <mandarining0918@gmail.com>
2021-07-13 20:39:13 +0900

    Feat: Content 작성, timezone추가

--- rooms/views.py
+++ rooms/views.py
@@ -1,4 +1,4 @@
-
+from django.utils import timezone
 from django.views.generic import ListView
 from . import models
 
@@ -12,3 +12,10 @@ class HomeView(ListView):  # Lendering 할 필요가 없어졌음
     paginate_by = 10
     paginate_orphans = 5 #이게 뭔지 정확히 모르겠음
     ordering = "created"
+    context_object_name = "rooms"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        now = timezone.now()
+        context["now"] = now
+        return context
