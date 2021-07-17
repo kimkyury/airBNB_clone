526be3b253f427698a64a44b298754d3b37c02b5
kimkyury <mandarining0918@gmail.com>
2021-07-17 11:56:30 +0900

    Feat: 조건부 검색기능 추가

--- rooms/views.py
+++ rooms/views.py
@@ -1,17 +1,16 @@
 from django.views.generic import ListView, DetailView
 from django.shortcuts import render
 from django_countries import countries
-
 from . import models
 
 
-class HomeView(ListView): # Lendering 할 필요가 없어졌음
-    
+class HomeView(ListView):
+
     """ HomeView Definition """
 
     model = models.Room
     paginate_by = 10
-    paginate_orphans = 5 #이게 뭔지 정확히 모르겠음
+    paginate_orphans = 5
     ordering = "created"
     context_object_name = "rooms"
 
@@ -19,7 +18,7 @@ class HomeView(ListView): # Lendering 할 필요가 없어졌음
 class RoomDetail(DetailView):
 
     """ RoomDetail Definition """
-    
+
     model = models.Room
 
 
@@ -28,44 +27,52 @@ def search(request):
     city = str.capitalize(city)
     country = request.GET.get("country", "KR")
     room_type = int(request.GET.get("room_type", 0))
-    
     price = int(request.GET.get("price", 0))
     guests = int(request.GET.get("guests", 0))
     bedrooms = int(request.GET.get("bedrooms", 0))
     beds = int(request.GET.get("beds", 0))
     baths = int(request.GET.get("baths", 0))
-    s_amenities = request.GET.get("amenities")
-    s_facilities = request.GET.get("facilities")
-
+    instant = request.GET.get("instant", False)
+    super_host = request.GET.get("super_host", False)
+    s_amenities = request.GET.getlist("amenities")
+    s_facilities = request.GET.getlist("facilities")
 
     form = {
-            "city": city, 
-            "s_country":country,
-            "s_room_type":room_type,
-            "price": price,
-            "guests": guests,
-            "bedrooms": bedrooms,
-            "beds": beds,
-            "baths": baths,
+        "city": city,
+        "s_room_type": room_type,
+        "s_country": country,
+        "price": price,
+        "guests": guests,
+        "bedrooms": bedrooms,
+        "beds": beds,
+        "baths": baths,
+        "s_amenities": s_amenities,
+        "s_facilities": s_facilities,
+        "instant": instant,
+        "super_host": super_host,
     }
+
     room_types = models.RoomType.objects.all()
     amenities = models.Amenity.objects.all()
     facilities = models.Facility.objects.all()
 
-
     choices = {
-            "countries":countries, 
-            "room_types": room_types,
-            "amenities" : amenities,
-            "facilities" : facilities, 
+        "countries": countries,
+        "room_types": room_types,
+        "amenities": amenities,
+        "facilities": facilities,
     }
 
+    filter_args = {}
 
+    if city != "Anywhere":
+        filter_args["city__startswith"] = city
 
-    return render(
-        request, 
-        "rooms/search.html", 
-        { **form, **choices
-        },
-    )
+    filter_args["country"] = country
 
+    if room_type != 0:
+        filter_args["room_type__pk"] = room_type
+
+    rooms = models.Room.objects.filter(**filter_args)
+
+    return render(request, "rooms/search.html", {**form, **choices, "rooms": rooms})
