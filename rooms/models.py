f19e34a92d067cb85d1e288169962b2ee1e6a805
kimkyury <mandarining0918@gmail.com>
2021-07-18 10:37:44 +0900

    Feat: 장고 class기반 form작성, 페이징 기능 추가

--- rooms/models.py
+++ rooms/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.urls import reverse
 from django_countries.fields import CountryField
 from core import models as core_models
-# Create your models here.
 
 
 class AbstractItem(core_models.TimeStampedModel):
@@ -19,37 +18,43 @@ class AbstractItem(core_models.TimeStampedModel):
 
 
 class RoomType(AbstractItem):
-    """  RoomType Model Definition """
+
+    """ RoomType Model Definition """
 
     class Meta:
         verbose_name = "Room Type"
 
 
 class Amenity(AbstractItem):
-    """  Amenity Model Definition """
-    
+
+    """ Amenity Model Definition """
+
     class Meta:
         verbose_name_plural = "Amenities"
 
 
 class Facility(AbstractItem):
-    """  Facility Model Definition """
-    
+
+    """ Facility Model Definition """
+
+    pass
+
     class Meta:
         verbose_name_plural = "Facilities"
 
+
 class HouseRule(AbstractItem):
-    """  HouseRule Model Definition """
-    
+
+    """ HouseRule Model Definition """
+
     class Meta:
         verbose_name = "House Rule"
-    
 
 
 class Photo(core_models.TimeStampedModel):
 
     """ Photo Model Definition """
-    
+
     caption = models.CharField(max_length=80)
     file = models.ImageField(upload_to="room_photos")
     room = models.ForeignKey("Room", related_name="photos", on_delete=models.CASCADE)
@@ -58,30 +63,32 @@ class Photo(core_models.TimeStampedModel):
         return self.caption
 
 
-
 class Room(core_models.TimeStampedModel):
 
-    """ Room model Definition """
-    name = models.CharField(max_length = 140)
+    """ Room Model Definition """
+
+    name = models.CharField(max_length=140)
     description = models.TextField()
     country = CountryField()
     city = models.CharField(max_length=80)
     price = models.IntegerField()
     address = models.CharField(max_length=140)
-    guests = models.IntegerField()
+    guests = models.IntegerField(help_text="How many people will be staying?")
     beds = models.IntegerField()
     bedrooms = models.IntegerField()
     baths = models.IntegerField()
     check_in = models.TimeField()
     check_out = models.TimeField()
     instant_book = models.BooleanField(default=False)
-    host =  models.ForeignKey("users.User", related_name="rooms", on_delete=models.CASCADE)
-    room_type = models.ForeignKey("RoomType", related_name="rooms", on_delete=models.SET_NULL, null = True)
-    
-    amenities = models.ManyToManyField("Amenity", related_name="rooms",blank=True)
-    facilities = models.ManyToManyField("Facility" , related_name="rooms",blank=True)
-    house_rules = models.ManyToManyField("HouseRule" , related_name="rooms", blank=True)
-
+    host = models.ForeignKey(
+        "users.User", related_name="rooms", on_delete=models.CASCADE
+    )
+    room_type = models.ForeignKey(
+        "RoomType", related_name="rooms", on_delete=models.SET_NULL, null=True
+    )
+    amenities = models.ManyToManyField("Amenity", related_name="rooms", blank=True)
+    facilities = models.ManyToManyField("Facility", related_name="rooms", blank=True)
+    house_rules = models.ManyToManyField("HouseRule", related_name="rooms", blank=True)
 
     def __str__(self):
         return self.name
